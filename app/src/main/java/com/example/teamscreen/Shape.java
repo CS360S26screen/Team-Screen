722ec11233d3f2b374fd4eb13c8f5ddad7ff9045
Shayan-Ayaz-Khan <shayanayazk@gmail.com>
2026-02-17 16:20:27 +0500

    Add Red color property to Shape class

--- app/src/main/java/com/example/teamscreen/Shape.java
+++ app/src/main/java/com/example/teamscreen/Shape.java
@@ -3,6 +3,7 @@ package com.example.teamscreen;
 public abstract class Shape {
     protected int x;
     protected int y;
+    String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
